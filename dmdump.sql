73e32a8c8e01c6029750ae9e5d734a9df77d39eb
kuldeep <kuldeepshekhawatcs@gmail.com>
2016-11-10 19:02:21 +0530

    dump

---
+++ dmdump.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.1.56-community : Database - dmnews
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dmnews` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `dmnews`;
+
+/*Table structure for table `news` */
+
+DROP TABLE IF EXISTS `news`;
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `location` int(11) DEFAULT NULL,
+  `newsDate` datetime DEFAULT NULL,
+  `newsDespription` varchar(255) DEFAULT NULL,
+  `newsHeadLine` varchar(255) DEFAULT NULL,
+  `image` longblob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+/*Data for the table `news` */
+
+insert  into `news`(`id`,`location`,`newsDate`,`newsDespription`,`newsHeadLine`,`image`) values (11,0,'2016-11-10 15:33:10','In India 500 & 1000 ruppess notes are banged!!!!!!!!!!!!!!','The <context:component-scan...> tag will be use to activate Spring MVC annotation scanning capability which allows to make use of annotations like @Controller and @RequestMapping etc.',NULL),(12,0,'2016-11-10 15:33:54','In America Trump has won the election','The <context:component-scan...> tag will be use to activate Spring MVC annotation scanning capability which allows to make use of annotations like @Controller and @RequestMapping etc.',NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
